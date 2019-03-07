47b1f2318c4ef54e82b5305e4fdcc969405edb15
Norodio <tom.arnaud97@gmail.com>
2019-03-07 11:22:05 +0100

    1st project part done

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(BodyParser.urlencoded({ extended: true }));
 
 var database, collection;
 
-app.listen(3000, () => {
+app.listen(9292, () => {
     MongoClient.connect(CONNECTION_URL, { useNewUrlParser: true }, (error, client) => {
         if(error) {
             throw error;
@@ -32,16 +32,49 @@ app.listen(3000, () => {
 
 app.get("/movies/populate", async (request, response) => {
     const movies = await imdb(DENZEL_IMDB_ID);
-    collection.insert(movies, (error, result) => {
+    collection.insertMany(movies, (error, result) => {
         if(error) {
             return response.status(500).send(error);
         }
         response.send(result.result);
+    });
+});
+
+app.get("/movies/cleandb" , (request, response)=> {
+  collection.deleteMany({}, (error, result)=>{
+    if(error){
+      return response.status(500).send(error);
+    }
+    response.send(result);
+  })
+});
+
+app.get("/movies/search", (request, response) => {
+    let limit = 5;
+    let meta = 0;
+    if(String(request.query.metascore) != "undefined"){
+      meta = request.query.metascore;
+    }
+    if(String(request.query.limit) != "undefined"){
+      limit = request.query.limit;
+    }
+    console.log(limit);
+    collection.aggregate([ {$match : {metascore : {$gt:parseInt(meta)} }},{$sort : {"metascore" : -1}} ] ).limit(parseInt(limit)).toArray((error, result) => {
+        if(error) {
+            return response.status(500).send(error);
+        }
+
+        let resultat = {
+            limit: limit,
+            MetascoreMin: meta,
+            results: result
+        };
+        response.send(resultat);
     });
 });
 
 app.get("/movies", (request, response) => {
-    collection.aggregate([ {$match : {metascore : {$gt:70} }},{ $sample: { size: 1 } } ] ).toArray((error, result) => {
+    collection.aggregate([ {$match : {metascore : {$gt:70} }},{ $sample: { size: 1 }}]).toArray((error, result) => {
         if(error) {
             return response.status(500).send(error);
         }
@@ -49,10 +82,8 @@ app.get("/movies", (request, response) => {
     });
 });
 
-
-
-app.get("/movie/:id", (request, response) => {
-    collection.findOne({ "_id": new ObjectId(request.param.id) }, (error, result) => {
+app.get("/movies/:id", (request, response) => {
+    collection.findOne({"id": request.params.id}, (error, result) => {
         if(error) {
             return response.status(500).send(error);
         }
@@ -60,21 +91,11 @@ app.get("/movie/:id", (request, response) => {
     });
 });
 
-app.get("/movie/search", (request,response)=> {
-
-})
-
-
-//Marche pas
-app.post("/movie/:id", (request, response) => {
-  /*
-  date = request.form['date'];
-  review = request.form['review'];
-    collection.update({ "_id": new ObjectId(request.param.id) },{$set : {"date": date , "review": review}}, (error, result) => {
+app.post("/movies/:id", (request, response) => {
+      collection.updateOne({ "id": request.params.id },{$set : {"date": request.body.date , "review": request.body.review}}, (error, result) => {
         if(error) {
             return response.status(500).send(error);
         }
         response.send(result.result);
     });
-    */
 });
